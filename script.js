d0ce528454e31e02b3bccd0238c7817b91f4383d
WHORYZEN <kanishkshkl@gmail.com>
2024-06-29 19:45:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function firstChar(text) {
-  // your code here
+function firstChar(str) {
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== ' ') {
+            return str[i];
+        }
+    }
+    return '';
 }
 
-// Do not change the code below
+
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
